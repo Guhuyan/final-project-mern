c6ebca7d1318e4a235b447b349ff3cbe65e833ba
Billy Bright <billybright1992@gmail.com>
2019-10-27 17:43:55 -0700

    added an image to home page for the body incase rest of page doesnt load

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Body from "./components/Body/index";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Body/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Body() {
+  return (
+    <img
+      src="https://ak5.picdn.net/shutterstock/videos/31720855/thumb/1.jpg"
+      className="img-fluid"
+      alt="Responsive image"
+    ></img>
+  );
+}
+export default Body;
