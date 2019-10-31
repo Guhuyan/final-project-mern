7b4dd54ce624b5fe8627c1d19f2f8be1f05f6fea
Billy Bright <billybright1992@gmail.com>
2019-10-30 19:41:17 -0700

    search on nav bar taking input and putting into url

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import Search from "./Nav";
 
-function Header() {
-  return (
-    <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand">Stock App</a>
-      <form className="form-inline">
-        <button
-          className="btn btn-dark btn-outline-light mr-sm-2"
-          type="button"
-        >
-          Sign Up.
-        </button>
-        <button className="btn btn-dark btn-outline-light" type="button">
-          Log In
-        </button>
-      </form>
-      <form className="form-inline">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-light my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
-    </nav>
-  );
+class HeaderSearch extends Component {
+  state = {
+    search: ""
+  };
+
+  handleSearch = event => {
+    const search = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: search
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefalt();
+    console.log(event);
+  };
+
+  render() {
+    return (
+      <Search
+        text={this.state.search}
+        handleSearch={this.handleSearch}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
 }
-export default Header;
+
+export default HeaderSearch;
