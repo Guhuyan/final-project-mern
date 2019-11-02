b0a4d58feefa02a798135f560f36a98ffb6534a8
Billy Bright <billybright1992@gmail.com>
2019-11-02 13:33:02 -0700

    api is console.log the resposne of the json from NYT

--- routes/api/nyTimesApi.js
+++ routes/api/nyTimesApi.js
@@ -1,4 +1,4 @@
-// import axios from "axios";
+const axios = require("axios");
 const router = require("express").Router();
 require("dotenv");
 
@@ -7,6 +7,16 @@ router.route("/").get(function(req, res) {
   const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
   const SUBURL = "&page=1&sort=newest&api-key=";
   const APIKEY = process.env.NYTIMES_API_KEY;
-  res.json({ test: BASEURL + q + SUBURL + APIKEY });
+
+  console.log(BASEURL + q + SUBURL + APIKEY);
+  axios
+    .get(BASEURL + q + SUBURL + APIKEY)
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    });
 });
 module.exports = router;
