c197ba686f2b22cf9f5fa8c5323cc34f9ced0a54
Gue <guehyang@gmail.com>
2019-11-23 13:19:54 -0800

    Backend server.js app now send index.html to our production build. Changed root package.json to install and run build using prefix on client app.

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Passport middleware
