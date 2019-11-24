f365083713ac2c748819964d330f5dc5f33e24f7
Gue <guehyang@gmail.com>
2019-11-24 14:53:38 -0800

    Requiring path and added engine to server's package.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const routes = require("./routes");
@@ -16,14 +17,6 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build"))); // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
-
 // Passport middleware
 app.use(passport.initialize()); // Passport config
 require("./config/passport")(passport); // Routes
@@ -32,13 +25,20 @@ app.use("/api/users", users);
 // Add routes, both API and view
 app.use(routes);
 
+const port = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build"))); // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-
-const port = process.env.PORT || 3001;
 
 // Start the API server
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
