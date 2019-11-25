fd3b68193da5c767fe565993ce85f23747a4a119
Gue <guehyang@gmail.com>
2019-11-24 21:40:46 -0800

    Changing postbuild script

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ mongoose
 
 // Handle React routing, return all requests to React app
 if (process.env.NODE_ENV === "production") {
-  app.use("/static", express.static(path.join(__dirname, "client/build")));
+  app.use("/", express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
