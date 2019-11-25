ba3f8c84369a3f902143df10b060b70127ba7fe1
Gue <guehyang@gmail.com>
2019-11-24 20:06:50 -0800

    ...

--- server.js
+++ server.js
@@ -33,29 +33,10 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use("/static", express.static(path.join(__dirname, "client/build"))); // Handle React routing, return all requests to React app
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
-//   });8
-// }
-
-// Handle React routing, return all requests to React app
-// app.use("/static", express.static(path.join(__dirname, "client/build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-} else {
-  app.use(express.static(path.join(__dirname, "/client/public")));
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  app.use("/static", express.static(path.join(__dirname, "client/build"))); // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
