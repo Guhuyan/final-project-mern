fbecc7791b29972e851c85ef7b433fe6fc710eca
Gue <guehyang@gmail.com>
2019-11-25 18:20:36 -0800

    Adding npm install to root package.json

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ mongoose
 
 // Handle React routing, return all requests to React app
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
